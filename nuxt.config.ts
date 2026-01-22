bfe4b317702f7d02ff595e1afdcd3cb813d45985
Mirumo <mysteryperson520@gmail.com>
2026-01-22 23:22:54 +0800

    feat: modify base url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,10 @@ import { defineNuxtConfig } from 'nuxt/config'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/2026/',
+  },
+
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
