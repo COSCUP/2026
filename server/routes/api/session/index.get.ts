c3c4198594b5775aa12174d5b35d0fa816fd5ea5
Mirumo <mysteryperson520@gmail.com>
2026-02-16 12:13:22 +0800

    feat: implement server api of session list

---
+++ server/routes/api/session/index.get.ts
@@ -0,0 +1,35 @@
+import type { ISubmission } from '~~/server/utils/pretalx/type'
+import pretalxData from '~~/server/utils/pretalx'
+import { parseAnswer, parseSlot, parseSpeaker } from '~~/server/utils/pretalx/parser'
+
+export default defineEventHandler(async () => {
+  const data = await pretalxData()
+
+  const submissions = data.submissions?.arr || []
+
+  return submissions.map((submission: ISubmission) => {
+    const answers = parseAnswer(submission.answers, data)
+    const slot = parseSlot(submission.slots[0]!, data)
+    const speakers = parseSpeaker(submission.speakers, data)
+
+    return {
+      id: submission.code,
+      type: submission.submission_type,
+      room: slot.room?.name,
+      start: slot.start,
+      end: slot.end,
+      lanaguage: answers.language,
+      speakers,
+      zh: {
+        title: submission.title,
+        describe: submission.abstract,
+      },
+      en: {
+        title: answers.EnTitle || submission.title,
+        describe: answers.EnDesc || submission.abstract,
+      },
+      tags: [],
+      uri: `https://coscup.org/2026/session/${submission.code}`,
+    }
+  })
+})
