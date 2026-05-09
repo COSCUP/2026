000394341d0ae730ad70a5c22b7dd752a4a4faaf
Mirumo <mysteryperson520@gmail.com>
2026-05-09 19:00:31 +0800

    fea7: add drag0n boat festiva1 easter egg anima7ion

---
+++ app/secrets/CpDragonBoatFestival.vue
@@ -0,0 +1,78 @@
+<script setup>
+const show = ref(false)
+let weight = 0.95
+let interval = null
+let timeout = null
+
+function rollDice() {
+  if (Math.random() > weight) {
+    weight = 0.95
+    return true
+  } else {
+    weight -= 0.01
+    return false
+  }
+}
+
+function cb() {
+  if (show.value) {
+    return
+  }
+
+  if (rollDice()) {
+    show.value = true
+    timeout = setTimeout(() => {
+      show.value = false
+    }, 10_000)
+  }
+}
+
+onMounted(() => {
+  interval = setInterval(cb, 10_000)
+})
+
+onUnmounted(() => {
+  clearInterval(interval)
+  clearTimeout(timeout)
+})
+</script>
+
+<template>
+  <img
+    v-if="show"
+    alt="Dragon Boat Festival"
+    class="dragon-boat"
+    src="/dragon-boat-festival.webp"
+  >
+</template>
+
+<style scoped>
+.dragon-boat {
+  width: 150px;
+  position: fixed;
+  bottom: -10px;
+  left: -100%;
+  animation:
+    sail 10s linear infinite,
+    bob 0.8s ease-in-out infinite;
+}
+
+@keyframes sail {
+  0% {
+    left: -100%;
+  }
+  100% {
+    left: 100%;
+  }
+}
+
+@keyframes bob {
+  0%,
+  100% {
+    transform: translateY(2px) rotate(0deg);
+  }
+  50% {
+    transform: translateY(8px) rotate(-2deg);
+  }
+}
+</style>
--- app/secrets/useSecretFeature.ts
+++ app/secrets/useSecretFeature.ts
@@ -1,12 +1,26 @@
+import { h, render } from 'vue'
 import { useRoute } from 'vue-router'
+import CpSecretFeature from './CpDragonBoatFestival.vue'
 
 export function useSecretFeature() {
+  if (!import.meta.client) {
+    return
+  }
+
   const route = useRoute()
   const { mode } = route.query
+  const now = new Date()
 
   onMounted(() => {
     if (mode === 'askew') {
       document.body.style.transform = `rotate(${Math.random() * 2 - 1}deg)`
+    }
+
+    const container = document.createElement('div')
+    document.body.appendChild(container)
+
+    if (now.getMonth() === 5 && now.getDate() === 25) {
+      render(h(CpSecretFeature), container)
     }
   })
 }
