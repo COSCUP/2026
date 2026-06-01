342484aa7dbf6068f6851ead20e3350a7981f5f2
Yi-Jyun Pan <me@pan93.com>
2026-06-02 00:41:08 +0800

    feat: add session search filters

--- app/pages/session.vue
+++ app/pages/session.vue
@@ -2,10 +2,13 @@
 import { prerenderRoutes } from 'nuxt/app'
 import { useI18n } from 'vue-i18n'
 import CpSessionDaySelector from '~/components/feature/CpSessionDaySelector.vue'
+import CpSessionEmptyBanner from '~/components/feature/CpSessionEmptyBanner.vue'
+import CpSessionFilterBar from '~/components/feature/CpSessionFilterBar.vue'
 import CpSessionList from '~/components/feature/CpSessionList.vue'
 import CpSessionTable from '~/components/feature/CpSessionTable.vue'
+import { useSessionFilter } from '~/composables/useSessionFilter'
 
-const { t } = useI18n()
+const { locale, t } = useI18n()
 
 const { data } = await useFetch('/api/session')
 
@@ -15,6 +18,19 @@ const days = computed(() => Object.keys(data?.value ?? {}).sort())
 const selectedDay = computed({
   get: () => manualSelectedDay.value ?? days.value[0] ?? null,
   set: (value) => void (manualSelectedDay.value = value),
+})
+
+const {
+  searchQuery,
+  filteredSessions,
+  roomOptions,
+  tagOptions,
+  selectedRoomIds,
+  selectedTagIds,
+} = useSessionFilter({
+  sessionsByDay: data,
+  selectedDay,
+  locale,
 })
 
 prerenderRoutes(
@@ -32,31 +48,47 @@ definePageMeta({
   <main v-if="selectedDay">
     <NuxtPage />
 
-    <div class="flex flex-col-reverse sm:flex-col">
+    <section class="mb-6">
+      <!--
+        On desktop, the selector will be at the top of the page.
+        On mobile, it will be fixed to the bottom.
+      -->
       <CpSessionDaySelector
         v-model="selectedDay"
-        class="w-screen bottom-0 left-0 sticky z-10 sm:bottom-auto"
+        class="w-screen bottom-[env(safe-area-inset-bottom)] left-0 fixed z-10 sm:bottom-auto sm:sticky"
         :days="days"
       />
 
+      <CpSessionFilterBar
+        v-model:search-query="searchQuery"
+        v-model:selected-room-ids="selectedRoomIds"
+        v-model:selected-tag-ids="selectedTagIds"
+        class="p-4 left-0 sticky z-50"
+        :room-options="roomOptions"
+        :tag-options="tagOptions"
+      />
+
       <CpSessionList
+        v-if="filteredSessions.length > 0"
         class="sm:hidden"
-        :sessions="data?.[selectedDay] ?? []"
+        :sessions="filteredSessions"
       />
       <CpSessionTable
+        v-if="filteredSessions.length > 0"
         class="hidden sm:grid"
         :column-width="200"
         :day="selectedDay"
         :interval="5"
         :row-height="50"
-        :sessions="data?.[selectedDay] ?? []"
+        :sessions="filteredSessions"
         :time-range="['09:00', '17:30']"
       />
-    </div>
 
-    <p v-if="!data?.[selectedDay]?.length">
-      {{ t('noSession') }}
-    </p>
+      <CpSessionEmptyBanner
+        v-if="filteredSessions.length === 0"
+        class="mx-4"
+      />
+    </section>
   </main>
   <main v-else>
     <p>
