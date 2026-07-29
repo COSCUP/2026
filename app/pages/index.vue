8e96819f90ba2e4cd66fbe7492a4fd17572df697
Mirumo <mysteryperson520@gmail.com>
2026-07-30 04:51:18 +0800

    refactor: move landing layout to homepage layout

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,7 +4,7 @@ import { useI18n } from 'vue-i18n'
 const { t } = useI18n()
 
 definePageMeta({
-  layout: 'landing',
+  layout: 'homepage',
 })
 
 const eventStartAt = new Date('2026-08-08T09:00:00+08:00').getTime()
