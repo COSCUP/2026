ccf880c29cd3ab8d247cc4318f619bc9caa498b3
Yi-Jyun Pan <me@pan93.com>
2026-08-01 22:00:43 +0800

    feat: self-host remote images during build

--- server/api/ad.get.ts
+++ server/api/ad.get.ts
@@ -1,16 +1,6 @@
 import type { Ad } from '#shared/types/ad'
+import { selfHostedImageUrl } from '#server/utils/remoteImages'
 import { fetchSheet } from '../utils/sheets'
-
-function transformImageUrl(source: string) {
-  if (source.startsWith('https://drive.google.com/file/d/')) {
-    const id = source.split('/')[5]
-    const url = `https://drive.google.com/thumbnail?id=${id}&&sz=w4000`
-
-    return url
-  }
-
-  return source
-}
 
 export default defineEventHandler(async (): Promise<Ad[]> => {
   const adRows = await fetchSheet('ad')
@@ -18,8 +8,8 @@ export default defineEventHandler(async (): Promise<Ad[]> => {
 
   return ads.map(({ newsId, 'image:vertical': imageVertical, 'image:horizontal': imageHorizontal, link, weight }) => ({
     id: newsId,
-    imageVertical: transformImageUrl(imageVertical),
-    imageHorizontal: transformImageUrl(imageHorizontal),
+    imageVertical: selfHostedImageUrl(imageVertical),
+    imageHorizontal: selfHostedImageUrl(imageHorizontal),
     link,
     weight,
   }))
